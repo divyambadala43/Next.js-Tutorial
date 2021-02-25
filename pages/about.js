f45897906221aefe06336cedc5f72e2ac83f5c4b
Divyam <divyambadala43@gmail.com>
2021-02-25 23:59:35 +0530

    basics

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+/** @format */
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum
+        consequatur fugit nam ratione nihil ab reiciendis. Dolores alias
+        explicabo dolore obcaecati eum. Molestias explicabo dignissimos at
+        provident libero laboriosam! Distinctio ipsa similique ratione rem quo
+        cumque quos, culpa quibusdam quas ea? Repudiandae asperiores expedita
+        reiciendis quisquam ut exercitationem mollitia nobis id reprehenderit
+        odio amet, modi quis suscipit consequatur labore quam?
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/ninjas/test.js
@@ -0,0 +1,9 @@
+const Test = () => {
+    return ( 
+        <idv>
+            <h1>Test</h1>
+        </idv>
+     );
+}
+ 
+export default Test;
