c39fe1323d8ead3c85bbf578747c2329d9520d36
Divyam <divyambadala43@gmail.com>
2021-02-26 00:06:12 +0530

    add components

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+      <div>
+        <p>Copyright &#169; 2021 NextTut</p>
+      </div>
+    );
+}
+ 
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 /** @format */
 
 import Head from "next/head";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum
@@ -17,6 +20,7 @@ export default function Home() {
         consectetur sunt! Officia aliquid adipisci voluptates debitis
         accusantium.
       </p>
+      <Footer />
     </div>
   );
 }
