2e7d1502e082cda3a8757f7966580c911acd3e6a
Divyam <divyambadala43@gmail.com>
2021-02-26 11:34:35 +0530

    add dynamic pages part-2

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,22 @@
+export const getStaticPaths = async () => {
+    const res = await fetch('"https://jsonplaceholder.typicode.com/users"');
+    const data = await res.json();
+
+    const paths = data.map(ninja => {
+        return {
+            params: {
+                id: ninja.id.toString()
+            }
+        }
+    })
+
+    return {
+        paths,
+        fallback : false
+    }
+
+}
+
 const Details = () => {
     return ( 
         <h1>DETAILS PAGE</h1>
