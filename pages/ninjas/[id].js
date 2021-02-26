1b4409029e4edc1e0e7db0319b49b389d008087d
Divyam <divyambadala43@gmail.com>
2021-02-26 11:57:37 +0530

    display ninja detais using getStaticPaths

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,5 +1,5 @@
 export const getStaticPaths = async () => {
-    const res = await fetch('"https://jsonplaceholder.typicode.com/users"');
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
     const data = await res.json();
 
     const paths = data.map(ninja => {
@@ -17,9 +17,28 @@ export const getStaticPaths = async () => {
 
 }
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+    const data = await res.json();
+
+    return {
+        props: {
+            ninja: data
+        }
+    }
+}
+
+const Details = ({ninja}) => {
     return ( 
-        <h1>DETAILS PAGE</h1>
+        <div>
+            <h1>{ninja.name} </h1>
+            <p>{ ninja.email}</p>
+            <p>{ ninja.website}</p>
+            <p>{ ninja.address.city}</p>
+
+        </div>
+        
      );
 }
  
