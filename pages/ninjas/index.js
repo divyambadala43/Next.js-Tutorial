224a4a0d780f6f6eeeac9453ff2bfa1791fe89f0
Divyam <divyambadala43@gmail.com>
2021-02-26 10:41:40 +0530

    add ninja list using getStaticProps

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,9 +1,39 @@
-const Ninjas = () => {
-    return ( <div>
-        <h1>
+import Head from 'next/head'
+import styles from '../../styles/Ninjas.module.css'
+export const getStaticProps = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+
+    return {
+        props: {
+            ninjas: data
+        }
+    }
+}
+
+const Ninjas = ({ninjas}) => {
+    return (
+      <>
+        <Head>
+          <title>Ninja List | Ninjas</title>
+          <meta name='keywords' content='ninjas' />
+        </Head>
+        <div>
+          <h1>
             All Ninjas
-        </h1>
-    </div> );
+            {ninjas.map((ninja) => {
+              return (
+                <div key={ninjas.id}>
+                  <a className={styles.single}>
+                    <h3>{ninja.name}</h3>
+                  </a>
+                </div>
+              );
+            })}
+          </h1>
+        </div>
+      </>
+    );
 }
  
 export default Ninjas;
